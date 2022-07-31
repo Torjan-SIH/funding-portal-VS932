9c97ab0767607c9d2b7515e00b5d10a8d067f3fe
Amani <vvit20bq1a05c8@gmail.com>
2022-07-31 20:20:26 +0530

    mod4

--- src/pages/funds/fundsprofile.js
+++ src/pages/funds/fundsprofile.js
@@ -16,24 +16,29 @@ const FundsProfile = () =>
                 <h1 className="fundProfileHeading">Fund Profile</h1>
                 <table className="fundProfileTable">
                     <tr>
-                        <th>Name: </th>
+                        <th><h3>Name: </h3></th><td></td>
                         <td><input type="text" name="oename" ></input></td>
                     </tr>
+                   <br></br>
                     <tr>
-                        <th>Email: </th>
+                        <th><h3>Email: </h3></th><td></td>
                         <td><input type="email" name="oemail" ></input></td>
                     </tr>
+                    <br></br>
                     <tr>
-                        <th>Contact: </th>
+                        <th><h3>Contact: </h3></th><td></td>
                         <td><input type="number" name="oecontact" ></input></td>
                     </tr>
+                    <br></br>
                     <tr>
-                        <th>Address: </th>
+                        <th><h3>Address: </h3></th><br></br>
                         <td><input type="text" name="oeaddress" ></input></td>
                     </tr>
-                    <tr>
-                        <td><button >Save</button></td>
-                        <td><button >Reset</button></td>
+                    <br></br>
+                    <tr><td></td>
+                    <td><center><button ><b>Save</b></button></center></td>
+                    <td><center><button ><b>Reset</b></button></center></td>
+                    
                     </tr>
                 </table>
             </form>
