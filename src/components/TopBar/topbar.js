6789c9eaca31ce1cf1ca2f3935c185992cf00223
divya g <aiswarya2002aishu@gmail.com>
2022-07-31 16:37:09 +0530

    search

--- src/components/TopBar/topbar.js
+++ src/components/TopBar/topbar.js
@@ -7,7 +7,12 @@ const TopBar = () =>{
         <div className="topBarContent">
             <table className="topBarTable">
                 <tr>
-                    <th>Welcome.... {}</th>
+                    <th> <input type="search" id="form1" class="form-control" />
+    <label class="form-label" for="form1">Search</label>
+  
+  <button type="button" class="btn btn-primary">
+    <i class="fas fa-search"></i>
+  </button></th>
                     <td><div><Link to="/">Logout</Link></div></td>
                 </tr>
             </table>
