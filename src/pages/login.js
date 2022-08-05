65b0738139e7000b7f0ec2dd8f97a81790020e63
SUMANTH <sumnth@SUMANTHs-MacBook-Pro.local>
2022-08-05 23:10:02 +0530

    alignment

--- src/pages/login.js
+++ src/pages/login.js
@@ -30,32 +30,33 @@ const Login = () =>{
          
    }
 
-      return(
-     <div class="contactform"><center>
-      <form onSubmit={submitLoginHandler} className="myform">
-         <table border="0" width="50%"  cellpadding="10" cellspacing="5">
-          <tr><th colSpan='2'><center><h4><b>LOG IN</b></h4></center></th></tr>
-          <tr className="loginUserName" >
-             <td colSpan={'2'}>
-                <input className="loginInput"  type="email" name="username" placeholder="Username"value={userLogin} required onChange = {(e) => changeLoginHandler(e)} ></input> 
-             </td>
-          </tr>
-          <tr className="loginPassword">
-               <td colSpan={'2'}>
-                  <input className="loginInput" type="password" placeholder="Password" name="password" value={passwdLogin} required onChange = {(e) => changeLoginHandler(e)}></input>
-               </td>
-          </tr>
-          <tr className="loginRoles">
-               <td>
+   return(
+      <div class="contactform"><center>
+         <form onSubmit={submitLoginHandler} className="myform">
+            <table className="loginTable" >
+               <tr>
+                  <th><center><h2><b>LOG IN</b></h2></center></th>
+               </tr>
+               <tr className="loginItem" >
+                  <td >
+                     <input className="loginInput"  type="email" name="username" placeholder="Username"value={userLogin} required onChange = {(e) => changeLoginHandler(e)} ></input> 
+                  </td>
+               </tr>
+               <tr className="loginItem">
+                  <td >
+                     <input className="loginInput" type="password" placeholder="Password" name="password" value={passwdLogin} required onChange = {(e) => changeLoginHandler(e)}></input>
+                  </td>
+               </tr>
+               <tr className="loginItem">
+                  <td>
                      <select className="loginInput" name="role" value={roleLogin} required onChange={(e) => changeLoginHandler(e)} > 
-                     <option name="role" value="">--Select--</option>
+                     <option name="role" value="">--Role--</option>
                      <option name="role" value="agencies" >Agencies</option>
                      <option name="role" value="hei" >HEI's</option>
                      <option name="role" value="oe" >OE's</option>
                      </select>
-                </td>
-          </tr>
-               
+                  </td>
+                </tr>
                <tr>
                   <td>
                      <center>
@@ -63,38 +64,32 @@ const Login = () =>{
                      </center>
                   </td>
                </tr>
-
-
-                <tr className="loginSubmit" >
-                  <td colSpan={2}>
+               <tr className="loginSubmit" >
+                  <td >
                      <center>
                         <button type="submit" className="loginSubmitButton"> Login</button> 
                      </center>
                   </td>
                </tr> 
-               
-
                <tr className="forgotPasswd">
-                  <td colSpan={2}>
+                  <td>
                      <center>
                         <Link to="/forgotpasswd" ><p style={{color:"black"}}>Forgot Password?</p></Link>
                      </center>
-               </td>
+                  </td>
                </tr>
                <tr className="loginRegister">
-               <td colSpan={2}>
+                  <td >
                      <center>
                         <h5>Don't have an account?</h5>
                      <h4><Link to="/register"><p style={{color:"black"}}>Sign-up</p></Link></h4>
                      </center>
                   </td>       
                </tr>
-               </table>
-               </form> 
+            </table>
+         </form> 
       </center>
-      </div>
-     
-      
+   </div> 
    )
    
 }
