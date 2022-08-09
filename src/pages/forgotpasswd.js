7510acb088111758279cdd8ab4080e0e220b3bda
gmk <gmk@example.com>
2022-08-09 10:28:17 +0530

    forgot password page

--- src/pages/forgotpasswd.js
+++ src/pages/forgotpasswd.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ForgotPasswd = () =>{
+   const [fEmail,setfEmail]=useState(null);
+   const setValue =(e) =>
+   {
+      setfEmail(e.target.value);
+   }
+   const submitForgotpassword =(e) =>
+   {
+      e.preventDefault();
+     
+   }
    return(
       <div className="forgotPassword" >
-         <h1>create new password</h1>
+         <form className="forgotPwd" onSubmit={submitForgotpassword}>
+          <center> <table>
+               <tr>
+            <input type="email" placeholder="Enter your registered mail ID" value={fEmail}  required onChange={(e) => setValue(e)}  name="forgotEmail"/>
+            </tr>
+            <tr>
+            <input type="submit" name="forgotButton" value="submit" />
+            </tr>
+            </table></center> 
+         </form>
       </div>
    )
 }
