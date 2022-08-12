095e30987dc246f93b653afdfcc11a99e568799b
Amani <vvit20bq1a05c8@gmail.com>
2022-08-12 13:46:14 +0530

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,23 +1,57 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./pagesStyle.css";
 
 
-const About = () => {
-  return (
-<body>
 
-<div className="c">
-  <h2><center>ONE NATION-ONE FUNDING</center></h2>
-</div>
-<div>
-  <h3>
-    Development of One Nation – One funding information portal which disseminates various funding schemes
-    with relevant details and interactive user interface across all funding agencies in higher education
- </h3>
-</div>
+const About = () =>{
+    
+  return(
+    <div>
 
-</body> 
+      <center><h3 className="abouth">"The main hope of education lies in the proper education of its youth"</h3></center>
+        <div className="sketchs">
+          <div>
+          <div className="sketch-1">
+            <h1><b>SECURE</b></h1><br></br>
+            <h2>This Website provides security for both the end users</h2>
+          </div>
+          <div className="sketch-1">
+            <img className="sketch11" src="https://cdn3d.iconscout.com/3d/premium/thumb/badge-4996114-4159572.png" alt=""/>
+          </div>
+          </div>
+
+          <div>
+          <div className="sketch-2">
+            <img className="sketch21" src="https://cdn3d.iconscout.com/3d/premium/thumb/badge-4996114-4159572.png" alt=""/>
+          </div>
+          <div className="sketch-2">
+            <h1><b>Easy to Use</b></h1><br></br>
+            <h2>This is easy to use for any application</h2>
+          </div>
+          </div>
+
+          <div>
+          <div className="sketch-3">
+            <h1><b>User Friendly</b></h1><br></br>
+            <h2>This Website provides user friendly for both the end users</h2>
+          </div>
+          <div className="sketch-3">
+            <img className="sketch3" src="https://cdn3d.iconscout.com/3d/premium/thumb/badge-4996114-4159572.png" alt=""/>
+          </div>
+          </div>
+
+
+          <div className="sketch-4">
+            <img className="sketch4" src="https://cdn3d.iconscout.com/3d/premium/thumb/badge-4996114-4159572.png" alt=""/>
+          </div>
+          <div className="sketch-4">
+            <h1><b>Accesible</b></h1><br></br>
+            <h2>This is easy Accessible for both the end users</h2>
+          </div>
+      </div>
+      </div>
   )
 }
-  
+
 export default About;
