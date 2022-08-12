50eb2d80b5e7bad8030c1e190907c7eecc811113
Amani <vvit20bq1a05c8@gmail.com>
2022-08-12 17:45:14 +0530

    navfotter

--- src/components/HeaderFooter/footer.js
+++ src/components/HeaderFooter/footer.js
@@ -4,9 +4,10 @@ import './headerFooterStyle.css';
 const Footers = () =>{
     return(
         <div className="footer">
-            <h4 className="footerH4">Thankyou for visiting!</h4>
-            <h4 className="footerH4">Contact us for any queries --&nbsp; email us on: torjanteam@gmail.com   &nbsp;&nbsp;&nbsp; contact no. : 0987654321,0863-23456</h4>
-        </div>
+            
+            <h4 className="footerH4"><center>Contact us for any queries --&nbsp; email us on: torjanteam@gmail.com   &nbsp;&nbsp;&nbsp; contact no. : 0987654321,0863-23456</center></h4>
+            
+            </div>
     )
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,12 +7,12 @@ const NavBar = () => {
     <div className="navBar">
         <ul className="navBarList" >
             <img class="logo" src='https://www.creditboss.in/wp-content/uploads/2020/10/Process-of-funding.jpg' alt=""/> 
-            <Link to="/home" ><li className="navList" id="onof"><h4><b>ONE NATION-ONE FUNDING</b></h4></li></Link>
-            <Link to="/home" ><li className="navList"><h4><b>Home</b></h4></li></Link>
-            <Link to="/about" ><li className="navList"><h4><b>About</b></h4></li></Link>
-            <Link to="/contact" ><li className="navList"><h4><b>Contact</b></h4></li></Link>
-            <Link to="/events" ><li className="navList"><h4><b>Events</b></h4></li></Link>
-            <Link to="/login" ><li className="navList"><h4><b>LogIn</b></h4></li></Link>
+            <Link to="/home" ><li className="navList" id="onof"><h2><b>ONE NATION-ONE FUNDING</b></h2></li></Link>
+            <Link to="/home" ><li className="navList"><h2><b>Home</b></h2></li></Link>
+            <Link to="/about" ><li className="navList"><h2><b>About</b></h2></li></Link>
+            <Link to="/contact" ><li className="navList"><h2><b>Contact</b></h2></li></Link>
+            <Link to="/contact" ><li className="navList"><h2><b>Events</b></h2></li></Link>
+            <Link to="/login" ><li className="navList"><h2><b>LogIn</b></h2></li></Link>
         </ul>
 
     </div>
